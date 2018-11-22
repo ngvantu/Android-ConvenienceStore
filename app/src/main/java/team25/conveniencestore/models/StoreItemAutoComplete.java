bfab3c510fd8de07e0fc76dbf07b0ccf4d05d964
LAPTOP-D6TPM65K\Tu Nguyen <ngvantu2912@gmail.com>
2018-11-22 15:59:39 +0700

    Custom item store auto complete

---
+++ app/src/main/java/team25/conveniencestore/models/StoreItemAutoComplete.java
@@ -0,0 +1,20 @@
+package team25.conveniencestore.models;
+
+public class StoreItemAutoComplete {
+
+    private String storeName;
+    private int logoImage;
+
+    public StoreItemAutoComplete(String name, int logoImage) {
+        this.storeName = name;
+        this.logoImage = logoImage;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public int getFlagImage() {
+        return logoImage;
+    }
+}
